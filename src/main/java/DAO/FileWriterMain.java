894f1bb1c69d2e524f7b40420dcd3329da4d6b1f
김정수 <tnrlaos@gmail.com>
2021-12-09 15:05:02 +0900

    phonebook.txt파일 만들기

--- src/main/java/DAO/FileWriterMain.java
+++ src/main/java/DAO/FileWriterMain.java
@@ -13,16 +13,19 @@ public class FileWriterMain {
 
 	public static void main(String[] args)  {
 		
+		String filePath="C:\\javaexercise\\PhoneBookServlet\\src\\main\\webapp/phoneBook.txt";
+		
 		try {
 		//FileWriter fw = new FileWriter(""); // 파일 경로 지정
 				
 		
-		PrintWriter pw = new PrintWriter("C:\\javaexercise\\PhoneBookServlet\\src\\main\\webapp/phoneBook.txt");
-		for(int i = 0; i < 9 ; i++) {
-			pw.println(new PhoneDTO("김정수"+i , "93050"+i ,"0101234123"+i));
-		}
-			//pw.write("10" +","+ "스티브리"+ "10" + ","+"01023412310"+", "+20);
-			pw.close();
+		PrintWriter pw = new PrintWriter(filePath);
+			
+			for(int i = 0; i < 9 ; i++) {
+				pw.println(new PhoneDTO("김정수"+i , "93050"+i ,"0101234123"+i));
+			}
+				
+				pw.close();
 			
 			
 			
